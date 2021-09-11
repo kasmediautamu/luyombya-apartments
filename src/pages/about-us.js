0ab1112d9307053606a055dd03de3a3c908e557c
kasmediautamu <ivan.khabuyhe1@gmail.com>
2021-09-11 10:51:58 +0300

    adds about-us and contact us page

---
+++ src/pages/about-us.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const AboutUsPage = () => {
+ return (
+     <div>
+         hi from About us page
+     </div>
+ )
+}
+export default AboutUsPage
---
+++ src/pages/contact-us.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const ContactPage = () => {
+ return (
+     <div>
+         hi from Contact us page
+     </div>
+ )
+}
+export default ContactPage
